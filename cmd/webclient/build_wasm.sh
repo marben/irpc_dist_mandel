b0123ac4556a9f9a1e5bcf5f926bd1a279483df3
Martin Benes <martinbenesh@gmail.com>
2026-02-18 10:30:00 +0100

    copy wasm_exec.js from go to static on each build to keep it in sync

--- cmd/webclient/build_wasm.sh
+++ cmd/webclient/build_wasm.sh
@@ -10,3 +10,7 @@ cd "$SCRIPT_DIR"
 GOOS=js GOARCH=wasm go build -o ../server/static/main.wasm .
 
 echo "✓ WASM compiled to ../server/static/main.wasm"
+
+cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ../server/static/
+
+echo "✓ Copied wasm_exec.js to ../server/static/"
